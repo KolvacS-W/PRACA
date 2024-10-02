f3e954877084db93097e6d2d722bb88bf626de26
Wujiaqi <425801729@qq.com>
2024-10-02 17:57:54 -0400

    simplify, stop using window.class

--- PRACA/public/classcode.js
+++ PRACA/public/classcode.js
@@ -1,18 +1,18 @@
 console.log('in class code')
 // create a house class that extends CSPY
-window.House = class House extends CSPY {
+class House extends CSPY {
 
     prompt = new Prompt("A House");
     roof_color = new Input("roof color");
     color = new Input("house color");
 }
 
-window.VictorianHouse = class VictorianHouse extends House {
+class VictorianHouse extends House {
     prompt = new Prompt("A Victorian House");
 }
 
 
-window.SimpleHouse = class SimpleHouse extends CSPY {
+class SimpleHouse extends CSPY {
     prompt = new Prompt("A Simple House with a triangular roof");
     house_color = new Input("house color");
     window_shape = new Input("Shape of the window, can be any SVG shape");
